0836396a56eda3fce09b9a4a372bcbe28bcea0d5
repplix <michael.repplinger@icloud.com>
2020-03-01 08:57:27 +0100

    added initial maven structure

---
+++ src/main/java/io/ddd/stereotyp/App.java
@@ -0,0 +1,13 @@
+package io.ddd.stereotyp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

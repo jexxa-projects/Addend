af011c44b08776b9ae5a97f74344f226083e7b10
repplix <michael.repplinger@icloud.com>
2020-03-07 18:25:50 +0100

    * Make AggregateID available during RUNTIME in order to find the method via reflection

--- src/main/java/io/ddd/stereotype/applicationcore/AggregateId.java
+++ src/main/java/io/ddd/stereotype/applicationcore/AggregateId.java
@@ -1,14 +1,17 @@
 package io.ddd.stereotype.applicationcore;
 
 import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
  * Represents the ID of an aggregate. Note that each @Aggregate needs exactly one @AggregateID
  */
 @Target(METHOD)
+@Retention(RUNTIME)
 @Documented
 public @interface AggregateId
 {

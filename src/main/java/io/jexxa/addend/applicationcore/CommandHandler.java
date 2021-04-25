75979c5f1cd7b13953e8efd85c610a3e93a0264f
repplix <michael.repplinger@icloud.com>
2021-04-25 07:27:06 +0200

    * Updated docu

--- src/main/java/io/jexxa/addend/applicationcore/CommandHandler.java
+++ src/main/java/io/jexxa/addend/applicationcore/CommandHandler.java
@@ -10,8 +10,7 @@ import java.lang.annotation.Target;
 /**
  * Represents a handling method for processing a command as it is used by {@link DomainWorkflow}.
  *
- *  Note: A {@link DomainEventHandler} has to be used if the functionality is not provided via an {@link ApplicationService}
- *  and must be initialized as a result of a {@link DomainEvent}
+ *  Note: The difference to a {@link DomainEventHandler} is typically used if the method is only triggered by a DomainEvent.
  */
 @Target(METHOD)
 @Retention(RUNTIME)
--- src/main/java/io/jexxa/addend/applicationcore/DomainEventHandler.java
+++ src/main/java/io/jexxa/addend/applicationcore/DomainEventHandler.java
@@ -10,12 +10,10 @@ import java.lang.annotation.Target;
 /**
  * Represents a handler for processing a {@link DomainEvent}.
  *
- * Note: A {@link DomainEventHandler} has to be used if the functionality is not provided via an {@link ApplicationService}
- * and must be initialized as a result of a {@link DomainEvent}. Otherwise you have to use {@link CommandHandler}
+ * Note: A {@link DomainEventHandler} has to be used if the functionality is triggered by a `DomainEvent`. Such a method typically must be
+ * utilized as a result of a {@link DomainEvent}. Otherwise you should use {@link CommandHandler}.
  *
- * Note: A {@link DomainEventHandler} is typically provided by a {@link DomainWorkflow} or a {@link DomainService}
  */
-
 @Target(METHOD)
 @Retention(RUNTIME)
 @Documented
